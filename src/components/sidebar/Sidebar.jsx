c82cee260c5246ba5e261dcc80be4c4d8c12680b
Adedamola Adeniyi <mcneo062@gmail.com>
2022-05-26 10:19:57 +0100

    Project completed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,12 +12,18 @@ import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSyst
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import { DarkModeContext } from '../../context/darkModeContext';
 
 const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
   return (
     <div className='sidebar'>
       <div className='top'>
-        <span className='logo'>Admin</span>
+        <Link to='/' style={{textDecoration: 'none'}}>
+          <span className='logo'>Admin</span>
+        </Link>
       </div>
       <hr/>
       <div className="center">
@@ -84,11 +90,11 @@ const Sidebar = () => {
       <div className="bottom">
         <div
           className="colorOption"
-          // onClick={() => dispatch({ type: "LIGHT" })}
+          onClick={() => dispatch({ type: "LIGHT" })}
         ></div>
         <div
           className="colorOption"
-          // onClick={() => dispatch({ type: "DARK" })}
+          onClick={() => dispatch({ type: "DARK" })}
         ></div>
       </div>
     </div>
